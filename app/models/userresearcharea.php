b3c1394124bef1df95aaab440f407f4c669b64bf
Habbes <haby_habbes@outlook.com>
2014-12-19 15:52:32 +0300

    Created UserResearchArea class to associate a researcher user with collaborative or thematic areas of research

---
+++ app/models/userresearcharea.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * class that associates a research area of specilization, either collaborative or thematic,
+ * for users (researchers)
+ * @author Habbes
+ *
+ */
+class UserResearchArea extends DBModel
+{
+	protected $user_id;
+	protected $group;
+	protected $type;
+	
+	private $_user;
+	
+	const THEMATIC = 1;
+	const COLLABORATIVE = 2;
+	
+	/**
+	 * 
+	 * @param User $user
+	 * @param int $group PaperGroup
+	 * @param type $type THEMATIC or COLLABORATIVE
+	 * @return UserResearchArea
+	 */
+	public static function create($user, $group, $type)
+	{
+		$r = new static();
+		$r->user_id = $user->getId();
+		$r->_user = $user;
+		$r->group = $group;
+		$r->type = $type;
+		return $r->save();
+	}
+	
+	/**
+	 * 
+	 * @param User $user
+	 * @param int $group PaperGroup
+	 * @return UserResearchArea
+	 */
+	public static function createThematic($user, $group)
+	{
+		return static::create($user, $group, self::THEMATIC);
+	}
+	
+	/**
+	 * 
+	 * @param User $user
+	 * @param int $group PaperGroup
+	 * @return UserResearchArea
+	 */
+	public static function createCollaborative($user, $group)
+	{
+		return static::create($user, $group, self::COLLABORATIVE);
+	}
+	
+	/**
+	 * 
+	 * @param User $user
+	 * @return array(UserResearchArea)
+	 */
+	public static function findThematicByUser($user)
+	{
+		return static::findAll("user_id=? AND type=?",[$user->getId(), (int) $this->type]);
+	}
+	
+	/**
+	 * 
+	 * @param User $user
+	 * @return array(UserResearchArea)
+	 */
+	public static function findCollaborativeByUser($user)
+	{
+		return static::findAll("user_id=? AND type=?", [$user->getId(), (int) $this->type]);
+	}
+}
