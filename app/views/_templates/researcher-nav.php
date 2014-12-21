10fb38e94718ad506509e125938269290d96858b
grafitto <nderitukelvin19@gmail.com>
2014-12-21 17:04:11 +0300

    added icons at the navigation bar

--- app/views/_templates/researcher-nav.php
+++ app/views/_templates/researcher-nav.php
@@ -1,10 +1,10 @@
 <div class="col-sm-3 col-md-2 sidebar">
-	<div class="nav nav-sidebar" id="titles">Notifications</div>
+	<div class="nav nav-sidebar" id="titles"><span class="glyphicon glyphicon-envelope"></span>&nbsp;Notifications</div>
 		<ul class="nav nav-sidebar">
 				<li class="active"><a href="#">All notifications<span class="sr-only">(current)</span></a></li>
 				<li><a href="#">Unread</a></li>
 		</ul>
-		<div class="nav nav-sidebar" id="titles">Papers</div>
+		<div class="nav nav-sidebar" id="titles"><span class="glyphicon glyphicon-file"></span>&nbsp;Papers</div>
 		<ul class="nav nav-sidebar">
 			<li><a href="">Submit papers</a></li>
 			<li><a href="">All papers</a></li>
@@ -12,7 +12,7 @@
 			<li><a href="">Projects</a></li>
 			<li><a href="">Final reports</a></li>
 		</ul>
-		<div class="nav nav-sidebar" id="titles">Workshop</div>
+		<div class="nav nav-sidebar" id="titles"><span class="glyphicon glyphicon-cog"></span>&nbsp;Workshop</div>
 		<ul class="nav nav-sidebar">
 			 <li><a href="">Manage</a></li>
 		</ul>
