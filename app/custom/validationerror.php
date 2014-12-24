1206fe5c6a02b53185374ffd253d5ea162894d8c
Habbes <haby_habbes@outlook.com>
2014-12-24 10:09:30 +0300

    Added validation errors

--- app/custom/validationerror.php
+++ app/custom/validationerror.php
@@ -8,17 +8,19 @@
 class ValidationError
 {
 	//User errors
-	const USER_USERNAME_INVALID = "UserNameInvalid";
-	const USER_PASSWORD_INVALID = "UserPasswordInvalid";
-	const USER_EMAIL_INVALID = "UserEmailInvalid";
-	const USER_USERNAME_UNAVAILABLE = "UserUsernameUnavailable";
-	const USER_EMAIL_UNAVAILABLE = "UserEmailUnavailable";
-	const USER_FIRST_NAME_EMPTY = "UserFirstNameEmpty";
-	const USER_LAST_NAME_EMPTY = "UserLastNameEmpty";
+	const USER_USERNAME_INVALID = "UsernameInvalid";
+	const USER_PASSWORD_INVALID = "PasswordInvalid";
+	const USER_PASSWORD_INCORRECT = "PasswordIncorrect";
+	const USER_PASSWORDS_DONT_MATCH = "PasswordsDontMatch";
+	const USER_EMAIL_INVALID = "EmailInvalid";
+	const USER_USERNAME_UNAVAILABLE = "UsernameUnavailable";
+	const USER_EMAIL_UNAVAILABLE = "EmailUnavailable";
+	const USER_FIRST_NAME_EMPTY = "FirstNameEmpty";
+	const USER_LAST_NAME_EMPTY = "LastNameEmpty";
 	const USER_TYPE_INVALID = "UserTypeInvalid";
-	const USER_ADDRESS_EMPTY = "UserAddressEmpty";
-	const USER_RESIDENCE_EMPTY = "UserResidenceEmpty";
-	const USER_NATIONALITY_EMPTY = "UserNationalityEmpty";
+	const USER_ADDRESS_EMPTY = "AddressEmpty";
+	const USER_RESIDENCE_EMPTY = "ResidenceEmpty";
+	const USER_NATIONALITY_EMPTY = "NationalityEmpty";
 	const USER_GENDER_INVALID = "UserGenderInvalid";
 	const PAPER_GROUP_INVALID = "PaperGroupInvalid";
 	
