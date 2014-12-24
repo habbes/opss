2a2e0fd386a039a53a74c0a881aba896db3cde41
Habbes <haby_habbes@outlook.com>
2014-12-24 10:04:45 +0300

    Edited constructor and added doc to ValidationException

--- app/core/validationexception.php
+++ app/core/validationexception.php
@@ -4,28 +4,52 @@ class ValidationException extends Exception
 {
 	private $validationErrors;
 	
-	public function __construct($message = null, $code = 0, $previous = null)
+	/**
+	 * 
+	 * @param array $errors
+	 * @param string $message
+	 * @param number $code
+	 * @param string $previous
+	 */
+	public function __construct($errors = [], $message = null, $code = 0, $previous = null)
 	{
-		$this->validationErrors = [];
+		$this->validationErrors = $errors;
 		parent::__construct($message, $code, $previous);
 		
 	}
 	
+	/**
+	 * 
+	 * @return array
+	 */
 	public function getErrors()
 	{
 		return $this->validationErrors;
 	}
 	
+	/**
+	 * 
+	 * @param array $errors
+	 */
 	public function setErrors($errors)
 	{
 		$this->validationErrors = $errors;
 	}
 	
+	/**
+	 * 
+	 * @param string $error
+	 */
 	public function addError($error)
 	{
 		$this->validationErrors[] = $error;
 	}
 	
+	/**
+	 * 
+	 * @param string $error
+	 * @return boolean
+	 */
 	public function hasError($error)
 	{
 		return array_searcher($error, $this->validationErrors);
