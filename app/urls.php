aff924447c7c33a675baa91aa5292953c623b745
Habbes <haby_habbes@outlook.com>
2014-12-25 20:39:45 +0300

    Created URL_ROOT_SUBPATH in urls in order to indicate the subpath used as root of the site This will be used to fix localRedirect(), on production servers, this should be set to empty

--- app/urls.php
+++ app/urls.php
@@ -1,3 +1,4 @@
 <?php
 
-define("URL_ROOT", "http://" . $_SERVER['SERVER_NAME'] . "/aerc_opss");
+define("URL_ROOT_SUBPATH", "/aerc_opss");
+define("URL_ROOT", "http://" . $_SERVER['SERVER_NAME'] . URL_ROOT_SUBPATH);
