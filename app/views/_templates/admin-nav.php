56e869b898145101d8fdb08edd846a34cd2318e3
Habbes <haby_habbes@outlook.com>
2015-01-06 20:53:52 +0300

    Changed sidebar Navigation look

--- app/views/_templates/admin-nav.php
+++ app/views/_templates/admin-nav.php
@@ -1,13 +1,16 @@
 <div class="nav nav-sidebar nav-list-header">
-	<span class="glyphicon glyphicon-envelope"></span>
-	<span>Notifications</span>
+	<span class="glyphicon glyphicon-envelope nav-icon"></span>
+	<span class="nav-list-header-text">Notifications</span>
 </div>
 <ul class="nav nav-sidebar nav-list-group">
 	<li class="nav-item active"><a href="messages/all">All notifications</a></li>
 	<li class="nav-item"><a href="messages/unread">Unread</a>
 </ul>
 
-<div class="nav nav-sidebar nav-list-header"><span class="glyphicon glyphicon-envelope"></span>Users</div>
+<div class="nav nav-sidebar nav-list-header">
+	<span class="glyphicon glyphicon-envelope nav-icon"></span>
+	<span class="nav-list-header-text">Users</span>
+</div>
 <ul class="nav nav-sidebar nav-list-group">
 	<li class="nav-item"><a href="users/invite">Invite</a></li>
 	<li class="nav-item"><a href="users/all">All</a>
