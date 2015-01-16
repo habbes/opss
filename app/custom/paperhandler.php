d6af43f18a23809032968029e30020e523435fd2
Habbes <haby_habbes@outlook.com>
2015-01-16 19:18:10 +0300

    Created PaperHandler as a base class for papers/* handlers

---
+++ app/custom/paperhandler.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * base class for handlers for pages for viewing and manipulating a paper
+ * @author Habbes
+ *
+ */
+abstract class PaperHandler extends LoggedInHandler
+{
+	
+	protected $paper;
+	
+	/**
+	 * ensures the user has access to this paper and redirects otherwise
+	 */
+	protected function assertPaperAccess()
+	{
+		if(!$this->user->getRole()->hasAccessToPaper($paper)){
+			$this->localRedirect("");
+		}
+	}
+	
+	protected function onCreate($identifier)
+	{
+		$paper = Paper::findByIdentifier($identifier);
+		if(!$paper)
+			$this->localRedirect("papers/all?not-found=true");
+		$this->paper = $paper;
+		parent::onCreate();
+		$this->assertPaperAccess();
+		
+	}
+}
