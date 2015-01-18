117acb3017fb505a6f151b53effa9282e8e80833
Habbes <haby_habbes@outlook.com>
2015-01-18 21:30:19 +0300

    Created VetReview

---
+++ app/models/vetreview.php
@@ -0,0 +1,165 @@
+<?php
+
+/**
+ * represents a review made by an admin during the vetting process
+ * @author Habbes
+ *
+ */
+class VetReview extends DBModel
+{
+	protected $paper_id;
+	protected $admin_id;
+	protected $status;
+	protected $verdict;
+	protected $date_initiated;
+	protected $date_submitted;
+	protected $comments;
+	
+	private $_admin;
+	private $_paper;
+	
+	//status constants
+	const STATUS_ONGOING = "ongoing";
+	const STATUS_COMPLETED = "completed";
+	
+	//verdict constants
+	const VERDICT_ACCEPTED = "accepted";
+	const VERDICT_REJECTED = "rejected";
+	
+	
+	
+	/**
+	 * 
+	 * @param Paper $paper
+	 * @return VetReview
+	 */
+	public static function create($paper)
+	{
+		$vr = new static();
+		$vr->_paper = $paper;
+		$vr->paper_id = $paper->getId();
+		$vr->date_initiated = Utils::dbDateFormat(time());
+		$vr->status = static::ONGOING;
+		
+		return $vr;
+	
+	}
+	
+	/**
+	 * 
+	 * @return Paper
+	 */
+	public function getPaper()
+	{
+		if(!$this->_paper)
+			$this->_paper = Paper::findById($this->paper_id);
+		return $this->_paper;
+	}
+	
+	/**
+	 * 
+	 * @return User
+	 */
+	public function getAdmin()
+	{
+		if(!$this->_admin)
+			$this->_admin = User::findById($this->admin_id);
+		return $this->_admin;
+	}
+	
+	/**
+	 * 
+	 * @param User $admin
+	 */
+	public function setAdmin($admin)
+	{
+		$this->_admin = $admin;
+		$this->admin_id = $admin->getId();
+	}
+	
+	/**
+	 * 
+	 * @return number timestamp
+	 */
+	public function getDateInitiated()
+	{
+		return strtotime($this->date_initiated);
+	}
+	
+	/**
+	 * 
+	 * @return number timestamp
+	 */
+	public function getDateSubmitted()
+	{
+		return strtotime($this->date_submitted);
+	}
+	
+	/**
+	 * 
+	 * @return string
+	 */
+	public function getComments()
+	{
+		return $this->comments;
+	}
+	
+	/**
+	 * 
+	 * @param string $comments
+	 */
+	public function setComments($comments)
+	{
+		$this->comments = $comments;
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getVerdict()
+	{
+		return $this->verdict();
+	}
+	
+	/**
+	 * 
+	 * @param Admin $admin
+	 * @param string $verdict one of the VERDICT_* constants
+	 * @param string $comments
+	 */
+	public function submit($admin, $verdict, $comments = "")
+	{
+		$this->setAdmin($admin);
+		if($comments)
+			$this->comments = $comments;
+		$this->verdict = $verdict;
+		$this->status = self::COMPLETED;
+		$this->date_submitted = Utils::dbDateFormat(time());
+		$this->save();
+		
+		if($verdict = self::REJECTED){
+			$this->getPaper()->setEditable(true);
+			$this->getPaper()->setStatus(Paper::STATUS_VETTING_REVISION);
+		}
+	}
+	
+	/**
+	 * 
+	 * @param Paper $paper
+	 * @return array(VetReview)
+	 */
+	public static function findByPaper($paper)
+	{
+		return static::findAllByField("paper_id", $paper->getId());
+	}
+	
+	/**
+	 * 
+	 * @param Paper $paper
+	 * @return VetReview
+	 */
+	public static function findCurrentByPaper($paper)
+	{
+		return static::findOne("paper_id=? AND status=?", [$paper->getId(), self::STATUS_ONGOING]);
+	}
+}
