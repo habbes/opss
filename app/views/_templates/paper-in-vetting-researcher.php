fa8466067f1c12811863e3c652b1297610193b71
Habbes <haby_habbes@outlook.com>
2015-01-19 06:47:33 +0300

    Changed heading of researcher's paper vetting notice

--- app/views/_templates/paper-in-vetting-researcher.php
+++ app/views/_templates/paper-in-vetting-researcher.php
@@ -1,5 +1,5 @@
 <div>
-	<h4>Paper is being vetted</h4>
+	<h4>Paper sent for vetting</h4>
 	<p>
 		Your paper has been sent to the secretariate for vetting. You will be notified when the process is complete.
 	</p>
