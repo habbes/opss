05c286d771084557a69b53b7c33fe9e5b879b281
Habbes <haby_habbes@outlook.com>
2015-02-01 16:23:33 +0300

    Added methods to save result message in session in BaseHandler

--- app/custom/basehandler.php
+++ app/custom/basehandler.php
@@ -17,4 +17,31 @@ class BaseHandler extends RequestHandler
 		$this->viewParams->resultMessage = $message;
 		$this->viewParams->resultMessageType = $type;
 	}
+	
+	/**
+	 * save result message in session
+	 * @param string $message
+	 * @param string $type
+	 */
+	protected function saveResultMessage($message, $type = "normal")
+	{
+		$this->session()->resultMessage = $message;
+		$this->session()->resultMessageType = $type;
+	}
+	
+	/**
+	 * set the result message that is saved in session
+	 * @param boolean $keep whether to keep this message in session, otherwise it is deleted
+	 */
+	protected function setSavedResultMessage($keep = false)
+	{
+		$msg = $this->session()->resultMessage;
+		if(!$msg) return;
+		$type = $this->session()->resultMessageType;
+		$this->setResultMessage($message, $type);
+		if(!$keep){
+			unset($this->session()->resultMessage);
+			unset($this->session()->resultMessageType);
+		}
+	}
 }
