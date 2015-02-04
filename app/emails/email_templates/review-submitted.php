493bed91d621b9415f0bb17900c51d2e24854538
Habbes <haby_habbes@outlook.com>
2015-02-04 22:23:23 +0300

    Fixed issue in review-submitted email template

--- app/emails/email_templates/review-submitted.php
+++ app/emails/email_templates/review-submitted.php
@@ -1,6 +1,6 @@
 <p>
 Dear {{name}}, <br>
 
-{{reviewer}} has submitted a review for the paper <a href="{{paperLink}}"><b>{{title}}</b> {{identifier}}</a>.<br>
+{{reviewer}} has submitted a review for the paper <a href="{{paperLink}}"><b>{{title}}</b> ({{identifier}})</a>.<br>
 The recommendation given was: <b>{{recommendation}}</b>.
 </p>
