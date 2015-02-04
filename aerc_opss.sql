9a4a29f9a172e2b10a36ddf9e001f2905e853347
Habbes <haby_habbes@outlook.com>
2015-02-04 23:28:59 +0300

    Updated database schema

--- aerc_opss.sql
+++ aerc_opss.sql
@@ -1,48 +1,46 @@
--- phpMyAdmin SQL Dump
--- version 4.0.4.1
--- http://www.phpmyadmin.net
+CREATE DATABASE  IF NOT EXISTS `aerc_opss` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `aerc_opss`;
+-- MySQL dump 10.13  Distrib 5.6.17, for Win32 (x86)
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 30, 2015 at 02:10 PM
--- Server version: 5.6.11
--- PHP Version: 5.5.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: aerc_opss
+-- ------------------------------------------------------
+-- Server version	5.6.11
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
---
--- Database: `aerc_opss`
---
-CREATE DATABASE IF NOT EXISTS `aerc_opss` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `aerc_opss`;
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `co_authors`
 --
 
-CREATE TABLE IF NOT EXISTS `co_authors` (
+DROP TABLE IF EXISTS `co_authors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `co_authors` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `date_added` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `email_activation`
 --
 
-CREATE TABLE IF NOT EXISTS `email_activation` (
+DROP TABLE IF EXISTS `email_activation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `email_activation` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `code` varchar(256) DEFAULT NULL,
   `expiry_date` datetime DEFAULT NULL,
@@ -51,30 +49,49 @@ CREATE TABLE IF NOT EXISTS `email_activation` (
   `email` varchar(255) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=40 ;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `files`
 --
 
-CREATE TABLE IF NOT EXISTS `files` (
+DROP TABLE IF EXISTS `files`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `files` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `filename` varchar(255) DEFAULT NULL,
   `inner_filename` varchar(255) DEFAULT NULL,
   `filetype` varchar(255) DEFAULT NULL,
   `directory` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=75 ;
+) ENGINE=InnoDB AUTO_INCREMENT=98 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Table structure for table `message_boxes`
+--
+
+DROP TABLE IF EXISTS `message_boxes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `message_boxes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `last_query_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=108 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `messages`
 --
 
-CREATE TABLE IF NOT EXISTS `messages` (
+DROP TABLE IF EXISTS `messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `messages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL,
   `subject` varchar(255) DEFAULT NULL,
@@ -84,28 +101,51 @@ CREATE TABLE IF NOT EXISTS `messages` (
   `message` text,
   `other_parts` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=101 ;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=200 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `message_boxes`
+-- Table structure for table `paper_authors`
 --
 
-CREATE TABLE IF NOT EXISTS `message_boxes` (
+DROP TABLE IF EXISTS `paper_authors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `paper_authors` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `last_query_time` datetime DEFAULT NULL,
+  `paper_id` int(11) DEFAULT NULL,
+  `author_id` int(11) DEFAULT NULL,
+  `date_added` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=108 ;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Table structure for table `paper_changes`
+--
+
+DROP TABLE IF EXISTS `paper_changes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `paper_changes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `action` varchar(255) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `revision` int(11) DEFAULT NULL,
+  `args` text,
+  `paper_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `papers`
 --
 
-CREATE TABLE IF NOT EXISTS `papers` (
+DROP TABLE IF EXISTS `papers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `papers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `identifier` varchar(100) DEFAULT NULL,
   `revision` int(11) DEFAULT NULL,
@@ -122,46 +162,19 @@ CREATE TABLE IF NOT EXISTS `papers` (
   `end_recallable_date` datetime DEFAULT NULL,
   `researcher_id` int(11) DEFAULT NULL,
   `other_parts` text,
+  `thematic_area` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `paper_authors`
---
-
-CREATE TABLE IF NOT EXISTS `paper_authors` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `paper_id` int(11) DEFAULT NULL,
-  `author_id` int(11) DEFAULT NULL,
-  `date_added` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `paper_changes`
---
-
-CREATE TABLE IF NOT EXISTS `paper_changes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `action` varchar(255) DEFAULT NULL,
-  `date` datetime DEFAULT NULL,
-  `revision` int(11) DEFAULT NULL,
-  `args` text,
-  `paper_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `reg_invitations`
 --
 
-CREATE TABLE IF NOT EXISTS `reg_invitations` (
+DROP TABLE IF EXISTS `reg_invitations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reg_invitations` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `admin_id` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
@@ -175,41 +188,87 @@ CREATE TABLE IF NOT EXISTS `reg_invitations` (
   `status` int(11) DEFAULT NULL,
   `registration_code` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Table structure for table `review_requests`
+--
+
+DROP TABLE IF EXISTS `review_requests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `review_requests` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `paper_id` int(11) DEFAULT NULL,
+  `reviewer_id` int(11) DEFAULT NULL,
+  `admin_id` int(11) DEFAULT NULL,
+  `status` varchar(100) DEFAULT NULL,
+  `date_sent` datetime DEFAULT NULL,
+  `date_responded` datetime DEFAULT NULL,
+  `permanent` tinyint(4) DEFAULT NULL,
+  `expiry_date` datetime DEFAULT NULL,
+  `response` varchar(100) DEFAULT NULL,
+  `response_text` text,
+  `request_text` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `reviews`
 --
 
-CREATE TABLE IF NOT EXISTS `reviews` (
+DROP TABLE IF EXISTS `reviews`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reviews` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `paper_id` int(11) DEFAULT NULL,
   `reviewer_id` int(11) DEFAULT NULL,
   `admin_id` int(11) DEFAULT NULL,
   `status` int(11) DEFAULT NULL,
   `recommendation` varchar(100) DEFAULT NULL,
-  `comments` text,
-  `researcher_comments` text,
-  `file_id` int(11) DEFAULT NULL,
-  `researcher_file_id` int(11) DEFAULT NULL,
+  `comments_to_admin` text,
+  `comments_to_author` text,
+  `file_to_admin_id` int(11) DEFAULT NULL,
+  `file_to_author_id` int(11) DEFAULT NULL,
   `date_submitted` datetime DEFAULT NULL,
   `permanent` tinyint(4) DEFAULT NULL,
   `due_date` datetime DEFAULT NULL,
   `posted` tinyint(4) DEFAULT NULL,
   `date_posted` datetime DEFAULT NULL,
   `date_initiated` datetime DEFAULT NULL,
+  `admin_comments` text,
+  `admin_file_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Table structure for table `user_research_areas`
+--
+
+DROP TABLE IF EXISTS `user_research_areas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_research_areas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `paper_group` int(11) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(80) DEFAULT NULL,
   `email` varchar(200) DEFAULT NULL,
@@ -225,29 +284,17 @@ CREATE TABLE IF NOT EXISTS `users` (
   `gender` tinyint(4) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=53 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_research_areas`
---
-
-CREATE TABLE IF NOT EXISTS `user_research_areas` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `paper_group` int(11) DEFAULT NULL,
-  `type` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33 ;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `vet_reviews`
 --
 
-CREATE TABLE IF NOT EXISTS `vet_reviews` (
+DROP TABLE IF EXISTS `vet_reviews`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vet_reviews` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `paper_id` int(11) DEFAULT NULL,
   `admin_id` int(11) DEFAULT NULL,
@@ -257,8 +304,16 @@ CREATE TABLE IF NOT EXISTS `vet_reviews` (
   `comments` text,
   `verdict` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-02-04 23:28:18
