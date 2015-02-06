50eb33cc86345de0fc73aff941f3458c9b1c9f85
Habbes <haby_habbes@outlook.com>
2015-02-06 13:03:46 +0300

    Created workshop review form

--- app/views/_templates/paper-workshop-queue-admin.php
+++ app/views/_templates/paper-workshop-queue-admin.php
@@ -1,6 +1,42 @@
+<?php 
+$formdata = $data->workshopReviewForm or $formdata = new DataObject();
+$formerror = $data->workshopReviewErrors or $formerror = new DataObject();
+?>
 <div>
 	<p>
 	The paper has been added to the presentation pipeline for the workshop scheduled on
 	<a class="link" href="<?=$data->workshop->getAbsoluteUrl()?>"><?= $data->workshop->toString()?></a>.
 	</p>
+	
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4 class="panel-title">Enter workshop review comments</h4>
+		</div>
+		<div class="panel-body">
+			<form enctype="multipart/form-data">
+				<div class="form-group">
+					<select class="form-control" name="verdict">
+						<option value="">Select conclusion</option>
+						<?php foreach(Review::getAdminVerdicts() as $verdict){?>
+						<option value="<?= $verdict?>"><?= Review::getVerdictString($verdict)?></option>
+						<?php } ?>
+					</select>
+					<span class="form-error help-block"><?= $formerror->verdict?></span>
+				</div>
+				<div class="form-group">
+					<textarea class="form-control" name="comments" rows="7" placeholder="Enter comments"></textarea>
+					<span class="form-error help-block"></span>
+				</div>
+				<div class="form-group">
+					<span>alternatively download comments file</span>
+					<input type="file" name="file"/>
+					<span class="form-error help-block"></span>
+				</div>
+				<div class="form-group">
+					<button class="btn btn-default">Submit Review</button>
+				</div>
+			
+			</form>
+		</div>
+	</div>
 </div>
