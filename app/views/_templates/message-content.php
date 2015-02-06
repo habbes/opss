903710b666c4e7a8a1fb8273dd9bfd99adcc1797
Habbes <haby_habbes@outlook.com>
2015-02-06 15:34:51 +0300

    Display links of attached parts in message reader

--- app/views/_templates/message-content.php
+++ app/views/_templates/message-content.php
@@ -1,6 +1,34 @@
 
-<div class="message-content">
+<div class="message-content margin-bottom">
 	<?= $data->message->getMessage()?>
+</div>
+<div class="message-parts margin-bottom">
+<?php foreach($data->message->getParts() as $part){?>
+	<div class="message-part">
+	<?php switch($part["type"]){
+		case Message::PART_PAPER:
+			$paper = Paper::findById($part["args"]["id"]) or $paper = new DataObject();
+	?>
+	<a class="link" href="<?= $paper->getAbsoluteUrl()?>">View Paper: <?= $paper->getIdentifier() ?></a>
+	<?php 
+			break;
+		case Message::PART_REVIEW_REQUEST:
+			$request = ReviewRequest::findById($part["args"]["id"]) or $request = new DataObject();
+	?>
+		<a class="link" href="<?= $request->getAbsoluteUrl()?>">View Request</a>
+	<?php 
+			break;
+		case Message::PART_USER:
+			$user = User::findById($part["args"]["id"]) or $user = new DataObject();
+	?>
+	<a class="link" href="<?= $user->getAbsoluteUrl()?>">View User: <?= $user->getFullName()?></a>
+	<?php 
+			break;
+		}
+	?>
+	
+	</div>
+<?php } ?>
 </div>
 <div class="message-time">
 	Sent on <?= Utils::siteDateTimeFormat($data->message->getDateSent())?>
