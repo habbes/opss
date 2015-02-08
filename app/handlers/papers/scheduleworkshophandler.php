d594723e18e46f1a4a72c650e2eabc0034e48c51
Habbes <haby_habbes@outlook.com>
2015-02-08 20:34:59 +0300

    Handled exceptions in add paper to queue

--- app/handlers/papers/scheduleworkshophandler.php
+++ app/handlers/papers/scheduleworkshophandler.php
@@ -10,7 +10,7 @@ class ScheduleWorkshopHandler extends PaperHandler
 	
 	public function showPage()
 	{
-		$this->viewParams = Workshops::findAll();
+		$this->viewParams->workshops = Workshop::findAll();
 		$this->renderView("papers/Home");
 	}
 	
@@ -34,17 +34,28 @@ class ScheduleWorkshopHandler extends PaperHandler
 		catch(OperationException $e)
 		{
 			$errors = new DataObject();
+			$msgSet = false;
 			foreach($e->getErrors() as $error){
 				switch($error){
 					case "WorkshopNotFound":
 						$errors->workshop = "The specified workshop was not found.";
 						break;
+					case OperationError::PAPER_NOT_PENDING:
+						$this->setResultMessage("The paper must be pending in order to be added to pipeline.", "error");
+						$msgSet = true;
+						break;
+					case OperationError::PAPER_ALREADY_IN_WORKSHOP:
+						$this->setResultMessage("The paper has already been added to a workshop review queue.", "error");
+						$msgSet = true;
+						break;
+						
 				}
 			}
 			
 			$this->viewParams->scheduleWorkshopErrors = $errors;
 			$this->viewParams->scheduleWorkshopForm = new DataObject($_POST);
-			$this->setResultMessage("Please correct the highlighted errors and try again.", "error");
+			if(!$msgSet)
+				$this->setResultMessage("Please correct the highlighted errors and try again.", "error");
 			$this->showPage();
 		}
 	}
