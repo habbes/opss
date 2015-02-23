e6d1cff038e31b989c2afd0083d01881f87e827c
Habbes <haby_habbes@outlook.com>
2015-02-23 11:02:04 +0300

    Hide researcher column in papers table for non-admins

--- app/views/_templates/papers-table.php
+++ app/views/_templates/papers-table.php
@@ -8,7 +8,9 @@
 		<thead>
 			<tr>
 				<th style="width:30%">Title</th>
+				<?php if($data->user->isAdmin()){?>
 				<th>Researcher</th>
+				<?php } ?>
 				<th>Level</th>
 				<th>Status</th>
 				<th>Date Submitted</th>
@@ -20,7 +22,9 @@
 				?>
 			<tr class="<?= $unread ?>">
 				<td><a class="link" href="<?=URL_PAPERS?>/<?=$paper->getIdentifier()?>"><?= escape($paper->getTitle())?> (<?= $paper->getIdentifier()?>)</a></td>
+				<?php if($data->user->isAdmin()){ ?>
 				<td><?= escape($paper->getResearcher()->getFullName())?></td>
+				<?php } ?>
 				<td><?= PaperLevel::getString($paper->getLevel())?></td>
 				<td><?= $paper->getStatusString()?></td>
 				<td><?= Utils::siteDateFormat($paper->getDateSubmitted()) ?></td>
