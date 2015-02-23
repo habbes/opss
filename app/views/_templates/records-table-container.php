a1925ffcd050aac7c844eba1848151b2c1ede5cb
Habbes <haby_habbes@outlook.com>
2015-02-23 10:58:08 +0300

    Fixed search/table positioning issue in IExplorer

--- app/views/_templates/records-table-container.php
+++ app/views/_templates/records-table-container.php
@@ -1,9 +1,8 @@
 <div class="records-group-container">
-	<div class="records-search-container col-sm-4 pull-right input-group">
+	<div class="records-search-container col-sm-4 col-sm-offset-8 input-group">
 		<input type="text" class="form-control search-field" id="<?= $data->searchFieldId?>" placeholder="Type to search" >
 		<span class="input-group-addon " id="basic-addon1"><span class="glyphicon glyphicon-search"></span></span>
 	</div>
-	<span class="clearfix"></span>
 	<div class="records-table-container" id="<?= $data->tableContainerId ?>" >
 		<?= $data->recordsTable ?>
 	</div>
