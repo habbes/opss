10d233af93c3654a786ecb7ce17bfa7b74ab49df
Habbes <haby_habbes@outlook.com>
2015-06-18 07:22:38 +0300

    Show profile picture in header only when available

--- app/views/_templates/base.php
+++ app/views/_templates/base.php
@@ -40,7 +40,10 @@
 				<ul class="nav navbar-nav navbar-right" id="username-dropdown">
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" 
-							aria-expanded="false"><img src="<?=$data->userPhoto?>" height="30">&nbsp;<?= $data->userName ?><span class="caret"></span></a>
+							aria-expanded="false"><?php if($data->user->getPhoto()){?>
+							<img src="<?=$data->userPhoto?>" height="30">&nbsp;
+							<?php } ?>
+							<?= $data->userName ?><span class="caret"></span></a>
 						<ul class="dropdown-menu" role="menu">
 							<li><a href="<?= URL_ROOT ?>/profile"><span class="glyphicon glyphicon-user"></span> My Profile</a></li>
 							<li class="divider"></li>
