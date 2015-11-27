98a769e117f2e82307056ad70b374aee627bf0c7
Habbes <haby_habbes@outlook.com>
2015-11-27 19:50:21 +0300

    Derive search field and table container id directory from search endpoint

--- app/views/reviewersview.php
+++ app/views/reviewersview.php
@@ -7,8 +7,6 @@ class ReviewersView extends LoggedInView{
 		
 		$this->data->searchEndpoint = "reviewers";
 		$this->data->searchUrl = "reviewers/search";
-		$this->data->searchFieldId = "reviewers-search-field";
-		$this->data->tableContainerId = "reviewers-table-container";
 		$this->data->recordsTable = $this->read("reviewers-table");
 		$this->data->pageContent = $this->read("records-table-container");
 		$this->showBase();
