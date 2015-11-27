6eed0937f5d5f1138825c43b50b7f4b01ced831c
Habbes <haby_habbes@outlook.com>
2015-11-27 20:14:35 +0300

    Added .env

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 
 require_once "vendor/autoload.php";
+
+// load environment variables from .env file
+if(file_exists(__DIR__ . DIRECTORY_SEPARATOR . '.ev')){
+	$envLoader = new josegonzales\Dotenv\Load(__DIR__. DIRECTORY_SEPARATOR . '.env');
+	$envLoader->parse()->toEnv();
+}
+
 require_once "app/dirs.php";
 require_once "app/autoload.php";
 require_once "app/routes.php";
